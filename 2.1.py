7880332116b37fc5a64f6130f8eb5c533216480c
Anastasia <asavina@yandex.ru>
2022-12-17 16:27:40 +0300

    init

--- 2.1.py
+++ 2.1.py
@@ -6,6 +6,7 @@ import numpy as np
 
 @profile
 def f():
+    """создаем список, копируем объект """
     x = list(range(10000))
     y = copy.deepcopy(x)
     return y
@@ -13,6 +14,7 @@ def f():
 
 @profile
 def f1():
+    """ Создаем список, копируем объект и удаляем созданный вначале список  """
     x = list(range(10000))
     y = copy.deepcopy(x)
     del x
@@ -21,12 +23,14 @@ def f1():
 
 @profile
 def f2():
+    """ Создаем список и отдаем его копию"""
     x = list(range(10000))
     return copy.deepcopy(x)
 
 
 @profile
 def f3():
+    """Создаем список, создаем второй список и заполняем его циклом"""
     x = list(range(10000))
     y = list()
     for t in x:
@@ -39,12 +43,14 @@ def f3():
 
 @profile
 def f4():
+    """ Создаем список циклом и возвращаем его"""
     x = [t for t in range(10000)]
     return x
 
 
 @profile
 def f5():
+    """Создаем пустой список и заполняем его циклом"""
     ls = list()
     for i in range(10000):
         ls.append(i)
@@ -70,3 +76,7 @@ print("Size 3 = {} bytes".format(sys.getsizeof(f3)))
 print("Size 4 = {} bytes".format(sys.getsizeof(f4)))
 print("Size 5 = {} bytes".format(sys.getsizeof(f5)))
 
+print("Size numpy arr = {} bytes".format(sys.getsizeof(arr)))
+print("Size список arr = {} bytes".format(sys.getsizeof(arr1)))
+print("Size кортеж arr = {} bytes".format(sys.getsizeof(arr2)))
+print("Size словарь arr = {} bytes".format(sys.getsizeof(arr3)))
