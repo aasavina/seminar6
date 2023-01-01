1fb2f6b8e1fcbfea5e45d4a691a66ca76ba89b48
Anastasia <asavina@yandex.ru>
2023-01-01 22:17:56 +0300

    init

--- seminar8/Zadanie3.py
+++ seminar8/Zadanie3.py
@@ -1,4 +1,4 @@
-#Задание 3.
+# Задание 3.
 # Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль.
 # Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 # в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
@@ -6,6 +6,8 @@
 class ZeroDivisionError(Exception):
     def __str__(self):
         return f'не делите на ноль'
+
+
 class Del:
     def __init__(self, ch, zn):
         self.ch = ch
@@ -15,7 +17,9 @@ class Del:
         if self.zn == 0:
             raise ZeroDivisionError
         else:
-            return self.ch/self.zn
+            return self.ch / self.zn
+
+
 d = Del(5, 0)
 try:
     print(d.dell())
