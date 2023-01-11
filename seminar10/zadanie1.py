5f36e2efedfae9bee2f2573badf6b7bae58ff9d4
Anastasia <asavina@yandex.ru>
2023-01-11 22:48:11 +0300

    init

--- seminar10/zadanie1.py
+++ seminar10/zadanie1.py
@@ -32,3 +32,31 @@ a = Road(10, 2000)
 a.material_amount()
 
 
+
+
+
+class Condition:
+
+    def __set__(self, instance, value):
+        if type(value) != str:
+            raise TypeError(f'введите тип данных строка')
+        instance.__dict__[self.my_attr] = value
+
+    def __set_name__(self, owner, my_attr):
+        self.my_attr = my_attr
+
+
+class Car:
+    name = Condition()
+    color = Condition()
+
+    def __init__(self, name, color, speed):
+        self.name = name
+        self.color = color
+        self.speed = speed
+
+
+car_1 = Car('Nissan', 'белый', 60)
+print(f'{car_1.name} - цвет {car_1.color}')
+
+
