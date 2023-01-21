79ed9bbe2ca5b158a6eaddfbd570dd08b149a243
Anastasia <asavina@yandex.ru>
2023-01-21 15:58:37 +0300

    init

--- lesson1_Savina/task_4.py
+++ lesson1_Savina/task_4.py
@@ -8,3 +8,11 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+SPISOK=["разработка", "администрирование", "protocol","standard"]
+for el in SPISOK:
+    print(f"'{el}' преобразуем в байты")
+    el_bytes=el.encode("UTF-8")
+    print(el_bytes)
+    print(f" '{el_bytes}' преобразуем в строку")
+    el_str=el_bytes.decode("UTF-8")
+    print(el_str)
