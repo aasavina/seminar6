ef39425bb340a74d8b506f29684c39f6b470d8ee
Anastasia <asavina@yandex.ru>
2023-01-21 16:05:57 +0300

    init

---
+++ lesson1_Savina/task_5.py
@@ -0,0 +1,20 @@
+"""
+Задание 5.
+
+Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
+преобразовать результаты из байтовового в строковый тип на кириллице.
+
+Подсказки:
+--- используйте модуль chardet, иначе задание не засчитается!!!
+"""
+
+import subprocess
+import chardet
+
+ARGS = ['ping', 'youtube.com']
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    result = chardet.detect(line)
+    print(result)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
